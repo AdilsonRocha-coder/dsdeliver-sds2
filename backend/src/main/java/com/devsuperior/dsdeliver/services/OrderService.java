bb505bed1a6d5b10ee170263ce5eab1f3dae4c6b
AdilsonRocha-coder <piuwave@hotmail.com>
2021-01-09 14:28:51 -0300

    GET /orders

--- backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
+++ backend/src/main/java/com/devsuperior/dsdeliver/services/OrderService.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dsdeliver.services;
 
+import java.time.Instant;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,12 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dsdeliver.dto.OrderDTO;
+import com.devsuperior.dsdeliver.dto.ProductDTO;
 import com.devsuperior.dsdeliver.entities.Order;
+import com.devsuperior.dsdeliver.entities.OrderStatus;
+import com.devsuperior.dsdeliver.entities.Product;
 import com.devsuperior.dsdeliver.repositories.OrderRepository;
+import com.devsuperior.dsdeliver.repositories.ProductRepository;
 
 @Service
 
@@ -18,10 +23,26 @@ public class OrderService {
 	@Autowired   /* o Autowired faz a resolução de dependência */
 	private OrderRepository repository;
 	
+	@Autowired
+	private ProductRepository productRepository;   /*aqui associaremos os produtos aos pedidos */
+	
 	@Transactional(readOnly=true)
 	public List<OrderDTO> findAll(){           /* aqui obteremos a lista de produtos do banco de dados */
 		List<Order> list = repository.findOrdersWithProducts(); 
 		return list.stream().map(x -> new OrderDTO(x)).collect(Collectors.toList());
 	}
+	
+	@Transactional
+	public OrderDTO insert(OrderDTO dto){     /* esse método retornará um objeto orderDTO correspondente ao pedido e seus itens inserido no BD */
+		Order order = new Order(null, dto.getAddress(), dto.getLatitude(), dto.getLongitude(), 
+				Instant.now(), OrderStatus.PENDING);
+		for (ProductDTO p : dto.getProducts()) {
+			Product product = productRepository.getOne(p.getId());
+			order.getProducts().add(product);
+		}
+		order = repository.save(order);  /* aqui salvaremos esse prdido/produto no BD */
+		return new OrderDTO(order);
+	}
+	
 
 }
