759c8c6ed3ea750c4c59c8fe98d66e32d220c21c
AdilsonRocha-coder <piuwave@hotmail.com>
2021-01-13 16:01:09 -0300

    Create front-web initial files

---
+++ sds2-aulas/front-web/src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css';
+ /* a função no react retorna um ou mais componentes. */
+function App() {
+  return (
+    <div>
+      <h1>Hello!</h1>
+    </div>
+  );
+}
+
+export default App;
