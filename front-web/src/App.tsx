987a6c3632a6655d8dc9a086708b3ae081ea5efd
AdilsonRocha-coder <piuwave@hotmail.com>
2021-01-13 17:45:42 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css';
+import Navbar from './Navbar';
  /* a função no react retorna um ou mais componentes. */
 function App() {
   return (
     <div>
-      <h1>Hello!</h1>
+      <Navbar />
     </div>
   );
 }
