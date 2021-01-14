83555c7e6e8b02f977aed9db9cf5dbd8de47aaaa
AdilsonRocha-coder <piuwave@hotmail.com>
2021-01-14 12:43:50 -0300

    Add Home page

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,10 +1,12 @@
-import './App.css';
-import Navbar from './Navbar';
- /* a função no react retorna um ou mais componentes. */
+import "./App.css";
+import Home from "./Home";
+import Navbar from "./Navbar";
+/* a função no react retorna um ou mais componentes. */
 function App() {
   return (
     <div>
       <Navbar />
+      <Home />
     </div>
   );
 }
