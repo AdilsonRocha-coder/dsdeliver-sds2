a36896e597fa296915885533613f1fc95e1434f1
AdilsonRocha-coder <piuwave@hotmail.com>
2021-01-19 17:28:46 -0300

    Finish front-web

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,9 +1,14 @@
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "./App.css";
 import Routes from "./Routes";
 /* a função no react retorna um ou mais componentes. */
 function App() {
   return (
-    <Routes />
+    <>
+      <Routes />
+      <ToastContainer />
+    </>
   );
 }
 
